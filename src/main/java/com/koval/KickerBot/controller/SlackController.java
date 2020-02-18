ab13c3201bba1a7ac84ca4ea135dc8b4fab7174f
H4dDock <maxolo.di@gmail.com>
2020-02-18 20:53:31 +0300

    ZeroDayCheck

--- src/main/java/com/koval/KickerBot/controller/SlackController.java
+++ src/main/java/com/koval/KickerBot/controller/SlackController.java
@@ -1,15 +1,24 @@
 package com.koval.KickerBot.controller;
 
-import com.koval.KickerBot.api.dto.ReceiveSlashDto;
 import com.koval.KickerBot.api.dto.SlackResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SlackController {
-    @PostMapping(value = "/slack/slash")
-    public SlackResponse onReceiveSlashCommand(@RequestBody ReceiveSlashDto receiveSlashDto){
+    @PostMapping(value = "/slack/slash",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public SlackResponse onReceiveSlashCommand(@RequestParam("team_id") String teamId,
+                                               @RequestParam("team_domain") String teamDomain,
+                                               @RequestParam("channel_id") String channelId,
+                                               @RequestParam("channel_name") String channelName,
+                                               @RequestParam("user_id") String userId,
+                                               @RequestParam("user_name") String userName,
+                                               @RequestParam("command") String command,
+                                               @RequestParam("text") String text,
+                                               @RequestParam("response_url") String responseUrl){
         return new SlackResponse("test text");
     }
 }
