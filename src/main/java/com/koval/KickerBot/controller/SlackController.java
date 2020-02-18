9914d7e9857d32a8d351cb1df553502c75d6adaa
H4dDock <maxolo.di@gmail.com>
2020-02-18 20:49:25 +0300

    ZeroDayCheck

--- src/main/java/com/koval/KickerBot/controller/SlackController.java
+++ src/main/java/com/koval/KickerBot/controller/SlackController.java
@@ -1,13 +1,15 @@
 package com.koval.KickerBot.controller;
 
+import com.koval.KickerBot.api.dto.ReceiveSlashDto;
 import com.koval.KickerBot.api.dto.SlackResponse;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SlackController {
     @PostMapping(value = "/slack/slash")
-    public SlackResponse onReceiveSlashCommand(){
+    public SlackResponse onReceiveSlashCommand(@RequestBody ReceiveSlashDto receiveSlashDto){
         return new SlackResponse("test text");
     }
 }
